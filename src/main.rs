b4bb931f63c638ce6ed2c2bddfcf097109462b05
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 14:39:36 -0300

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-fn main() { 
 
-
-    for number in (1..4).rev() {
-        println!("{number}!");
-    };
-
-    println!("LIFTOFF!!!");
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
+
+fn main() {
+    let n = 100;
+    for i in 0..n {
+        println!("{}", {fibonacci(i)});
+    }
+}
+
