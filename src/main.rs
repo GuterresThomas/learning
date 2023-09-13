c1ed8ad7f6230eefd9916e406edad577b2d1cfeb
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 13:07:03 -0300

    conditions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-    let y = five();
+fn main() { 
+    let number = 3;
 
-    println!("the value of y is: {y}");
-}
-
-
-fn five() -> i32 {
-    5
+    if number % 4 == 0{
+        println!("number is divisible by 4");
+    } else if number % 3 == 0{
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2")
+    };
 }
