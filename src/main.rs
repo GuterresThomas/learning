0aa285c238973896398d5a4faa553eb8245ee0ef
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 13:54:27 -0300

    for

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ fn main() {
 
     let a = [10, 20, 30, 40, 50];
 
-    let mut index = 0;
+    
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1; // ou index = index + 1
+    for element in a  {
+        println!("the value is:{element}");
     }
 }
