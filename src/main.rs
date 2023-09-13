9797d3826076eb86ca0df6e5cc8cf50b3b7c9dd7
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 14:50:16 -0300

    fibonacci otimizado

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 
-fn fibonacci(n: u32) -> u32 {
+fn fibonacci(n: u32) -> u64 {
     if n <= 1 {
-        n
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
+        return n as u64;
+    } 
+
+    let mut a = 0;
+    let mut b = 1;
+    let mut result = 0;
+
+    for _ in 2..=n {
+        result = a + b;
+        a = b;
+        b = result;
     }
+
+    result
 }
 
 fn main() {
-    let n = 100;
+    let n = 50;
     for i in 0..n {
         println!("{}", {fibonacci(i)});
     }
