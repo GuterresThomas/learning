7f3cc6f095aee598efeac72bf539e7e5e7c1bd1e
Guterresthomas <guterresmikael@gmail.com>
2023-09-12 21:26:39 -0300

    3.3 12/09

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+   
+   print_labeled_measurement(5, 'h');
+
+   
+}
+
+   
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("the measurement is: {value}{unit_label}");
+}
+   
+
