6cecccdf95abac13600d2da1a5b5ca8ca975579f
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 18:25:56 -0300

    fahrenheit to celsius

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
 
-fn fibonacci(n: u32) -> u64 {
-    if n <= 1 {
-        return n as u64;
-    } 
 
-    let mut a = 0;
-    let mut b = 1;
-    let mut result = 0;
 
-    for _ in 2..=n {
-        result = a + b;
-        a = b;
-        b = result;
-    }
 
-    result
-}
+
+
 
 fn main() {
-    let n = 50;
-    for i in 0..n {
-        println!("{}", {fibonacci(i)});
-    }
+   let mut fahrenheit: f64 = 6.0;
+
+   let celsius_factor: f64 = 5.0 / 9.0;
+
+   let celsius = (fahrenheit - 32.0) * celsius_factor;
+
+   println!("fahrenheit: {:.2}ºF", fahrenheit);  
+   println!("celsius: {:.2} ºC", celsius);  
 }
 
