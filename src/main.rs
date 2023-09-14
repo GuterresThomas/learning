384aacf41d27e8e19c23f7f0f2641e6f214c4170
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 21:32:07 -0300

    ownership

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 fn main()  {
-    let s1 = gives_ownership();
+
+    let s1 = String::from("hello");
     
-    let s2 = String::from("hello");
+    let(s2, len) = calculate_len(s1);
 
-    let s3 = takes_and_gives_back(s2);
-
-    fn gives_ownership() -> String {
-
-
-        let some_string = String::from("yours");
-
-        some_string
+    println!("The length of '{}' is '{}'", s2, len);
     }
 
-    fn takes_and_gives_back(a_string: String) -> String {
-        a_string
-    }
 
+fn calculate_len(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
+
+    
+
