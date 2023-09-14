d9e6abc59ef58300559ba386c58febd03ade186b
Guterresthomas <guterresmikael@gmail.com>
2023-09-14 10:21:59 -0300

    slice

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,8 @@
-
-fn first_word(s: &String) -> usize {
-
-
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' '{
-            return i;
-        }
-    }
-
-    s.len()
-}
-
-fn main()  {
-    let mut s = String::from("hello world");  
-
-    let word = first_word(&s);
-
-    s.clear()
-
-}
-
-
-
-
-
+fn main() {
+    let s = String::from("hello world");
     
+    let hello = &s[0..5];
+    let world = &s[6..11];
 
+
+}
