e0a3ad245fe8cf84f0ca0c875fa92f4d3a4f0b63
Guterresthomas <guterresmikael@gmail.com>
2023-09-14 10:51:54 -0300

    slicess

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 fn main() {
-    let s = String::from("hello world");
+
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+
+    let word = first_word(&my_string[..]);
+
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+
+    let word = first_word(&my_string_literal[..]);
+
+    let word = first_word(&my_string_literal);
+
+    first_word(&my_string);
     
-    let hello = &s[0..5];
-    let world = &s[6..11];
-
-
 }
