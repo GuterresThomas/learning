926da3edf45baf1e40197113e53c0246bc7dde5c
Guterresthomas <guterresmikael@gmail.com>
2023-09-13 21:21:49 -0300

    ownership

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+fn main()  {
+    let s1 = gives_ownership();
+    
+    let s2 = String::from("hello");
+
+    let s3 = takes_and_gives_back(s2);
+
+    fn gives_ownership() -> String {
 
 
+        let some_string = String::from("yours");
 
+        some_string
+    }
 
+    fn takes_and_gives_back(a_string: String) -> String {
+        a_string
+    }
 
-
-
-fn main() {
-   let mut fahrenheit: f64 = 6.0;
-
-   let celsius_factor: f64 = 5.0 / 9.0;
-
-   let celsius = (fahrenheit - 32.0) * celsius_factor;
-
-   println!("fahrenheit: {:.2}ºF", fahrenheit);  
-   println!("celsius: {:.2} ºC", celsius);  
 }
-
