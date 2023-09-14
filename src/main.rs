972e178b11fd3d15ab74dc8473caec54be6f8e8a
Guterresthomas <guterresmikael@gmail.com>
2023-09-14 11:00:33 -0300

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
+fn main () {
+    let a = [1, 2, 3, 4, 5];
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
+    let slice = &a[1..3];
 
-    &s[..]
-}
-
-fn main() {
-
-    let my_string = String::from("hello world");
-
-    let word = first_word(&my_string[0..6]);
-
-    let word = first_word(&my_string[..]);
-
-    let word = first_word(&my_string);
-
-    let my_string_literal = "hello world";
-
-    let word = first_word(&my_string_literal[0..6]);
-
-    let word = first_word(&my_string_literal[..]);
-
-    let word = first_word(&my_string_literal);
-
-    first_word(&my_string);
-    
+    assert_eq!(slice, &[2, 3]);
 }
