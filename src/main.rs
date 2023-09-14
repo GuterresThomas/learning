9d1353d2e80733b2d4016f1698026bc4a1007bad
Guterresthomas <guterresmikael@gmail.com>
2023-09-14 11:38:03 -0300

    tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,8 @@
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username: username,
-        email: email,
-        sign_in_count: 1
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 
-fn main () {
-
-    let user1 = User {
-        active: true,
-        username: String::from("John Deer"),
-        email: String::from("email@email.com"),
-        sign_in_count: 1,
-    };
-
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("anotheremail@email.com"),
-        sign_in_count: user1.sign_in_count, // equal to  email: String::from("anotheremail@email.com"), ..user1
-    };
-
-    
+fn main() {
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);    
 }
