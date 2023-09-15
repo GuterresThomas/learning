dc97ca98eae05ac2c8e98420956108f1da82a832
Guterresthomas <guterresmikael@gmail.com>
2023-09-15 19:31:41 -0300

    match and enum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    Quarter(UsState),
 }
 
 fn value_in_cents (coin: Coin) -> u8 {
@@ -13,10 +18,12 @@ fn value_in_cents (coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25},
     }
 }
 
 fn main () {
-
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 }
