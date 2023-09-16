862c229965bb6616e8ea7b051558923423821d87
Guterresthomas <guterresmikael@gmail.com>
2023-09-15 21:28:20 -0300

    if let

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents (coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        },
-    }
-}
 
 fn main () {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
+    let mut count = 0;
+    
+    if let Coin::Quarter (state) = coin {
+        println!("State Quarter from {}", state);
+    }else {
+        count += 1;
+    }
 }
