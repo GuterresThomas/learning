63a8eab636b4f98003acf65e7058868c0958e02c
Guterresthomas <guterresmikael@gmail.com>
2023-09-19 15:04:04 -0300

    Main result

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,7 @@
-use std::fs;
-use std::io::{self, Read};
+use std::fs::File;
+use std::error::Error;
 
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    /*let username_file_result = File::open("Hello.txt");
-
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e)
-    };
-
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
-    */
-
-    /*let mut username_file = File::open("Hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username)
-    */
-
-
-    /*let mut username = String::new();
-
-    File::open("Hello.txt")?.read_to_string(&mut username)?;
-    Ok(username)*/
-
-    fs::read_to_string("hello.txt")
-}
-
-fn main() {
-
+fn main() -> Result<(), Box<dyn Error>> {
+    let greeting_file = File::open("Hello.txt")?;
+    Ok(())
 }
