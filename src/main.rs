d2c709dc2480556479baf5be72fcd7902f55d0a9
Guterresthomas <guterresmikael@gmail.com>
2023-09-19 13:54:16 -0300

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,17 @@ fn read_username_from_file() -> Result<String, io::Error> {
     }
     */
 
-    let mut username_file = File::open("Hello.txt")?;
+    /*let mut username_file = File::open("Hello.txt")?;
     let mut username = String::new();
     username_file.read_to_string(&mut username)?;
     Ok(username)
+    */
 
+
+    let mut username = String::new();
+
+    File::open("Hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
 
 fn main() {
