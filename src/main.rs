081b01d732690b220e8d1044252f7e41b98b4abd
Guterresthomas <guterresmikael@gmail.com>
2023-09-19 10:30:57 -0300

    !format macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main () {
-   let mut s = String::from("Hello");
-   s.push_str("string");
+   let s1 = String::from("tic");
+   let s2: String = String::from("tac");
+   let s3: String = String::from("toe");
 
+   let s = format!("{}-{}-{}", s1, s2, s3);
    println!("{}", s)
 }
