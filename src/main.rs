de683de52dedc5bc5a24b21070b0deaf65ffc341
Guterresthomas <guterresmikael@gmail.com>
2023-09-18 22:12:04 -0300

    vec

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 fn main () {
-    let v = vec![1,2,3,4,5];
-
-    let third: &i32 = &v[2];
-    println!("The third element of v is {}", third);
-
-    let third:Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element of v is {}", third),
-        None => println!("There is no third element"),
+    let v = vec![100, 32, 57];
+    for i in v {
+        println!("{i}")
     }
+
+    
 
 
 }
