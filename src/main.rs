202aaea6c0db43f3ef8ea88e6267b30ae23d8e57
Guterresthomas <guterresmikael@gmail.com>
2023-09-19 13:49:47 -0300

    Error handling shortcut

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, Read};
 
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("Hello.txt");
+    /*let username_file_result = File::open("Hello.txt");
 
     let mut username_file = match username_file_result {
         Ok(file) => file,
@@ -16,6 +16,13 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+    */
+
+    let mut username_file = File::open("Hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+
 }
 
 fn main() {
