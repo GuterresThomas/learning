7f32bae79e5e2510d11155f052d0b549fc5566d2
Guterresthomas <guterresmikael@gmail.com>
2023-09-19 11:35:08 -0300

    panic!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use std::collections::HashMap;
-
 fn main () {
-    let text = "Hello world wonderful world";
+    let v = vec![1, 2, 3];
 
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-
-    println!("{:?}", map);
+    v[99];
 }
