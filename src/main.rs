03de57ff0e3c008c32dab80327b345e9f9aa7b7e
Guterresthomas <guterresmikael@gmail.com>
2023-09-20 14:52:18 -0300

    generic types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-struct Point<T> {
+/* struct Point<T> {
     x: T,
     y: T,
 }
 fn main() {
     let integer = Point {x: 5, y: 10};
     let float = Point {x: 1.0, y: 5.0}; //tem que ser o mesmo tipo
+} */
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let both_integer = Point {x: 5, y: 10};
+    let both_float = Point {x: 1.2, y:3.4};
+    let both_different = Point {x: 1.3, y: 5}; //agora funciona 
 }
